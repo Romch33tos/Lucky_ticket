ba73bcbecd24f1aeb9cada9aad3668f1d6d67aff
Roman Khadraliev <Romch33tos@yandex.ru>
2025-06-30 15:34:12 +0700

    Добавлены надписи на билете

--- bus-lucky_ticket.py
+++ bus-lucky_ticket.py
@@ -15,5 +15,46 @@ class LuckyTicket:
     self.create_widgets()
     self.window.mainloop()
 
+  def create_widgets(self):
+    Label(
+      self.window,
+      text="Минтранс РФ",
+      font=("Calibri", 10),
+      bg="snow1",
+      fg="chartreuse4"
+    ).grid(row=0, column=0, sticky=NW, padx=20, pady=5)
+
+    Label(
+      self.window,
+      text="АО «КТК»              ОДНА ПОЕЗДКА",
+      font=("Calibri", 12),
+      bg="snow1",
+      fg="chartreuse4"
+    ).grid(row=1, column=0, sticky=NW, padx=20, pady=5)
+
+    Label(
+      self.window,
+      text="АВТОБУС",
+      font=("Calibri", 14, 'bold'),
+      bg="snow1",
+      fg="chartreuse4"
+    ).grid(row=2, column=0, sticky=NW, padx=20, pady=5)
+
+    Label(
+      self.window,
+      text="ТЕРМИНАЛ (Транспортная карта)",
+      font=("Calibri", 12),
+      bg="snow1",
+      fg="chartreuse4"
+    ).grid(row=3, column=0, sticky=NW, padx=20, pady=5)
+
+    Label(
+      self.window,
+      text=random.choice(self.bus_numbers),  
+      font=("Calibri", 14),
+      bg="snow1",
+      fg="maroon1"
+    ).grid(row=4, column=0, sticky=NW, padx=40, pady=10)
+
 if __name__ == "__main__":
   app = LuckyTicket()
