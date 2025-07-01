b14a95db195b2f90220ef9a772676020e79b37be
Roman Khadraliev <Romch33tos@yandex.ru>
2025-07-01 21:45:53 +0700

    Настройка параметров окна

--- tram-lucky_ticket.py
+++ tram-lucky_ticket.py
@@ -4,6 +4,9 @@ class LuckyTramTicket:
   def __init__(self):
     self.window = Tk()
     self.window.title("Счастливый билет")
+    self.window.configure(bg='snow')
+    self.window.resizable(width=False, height=False)
+    self.window.geometry("360x220")
     self.window.mainloop()
 
 if __name__ == "__main__":
