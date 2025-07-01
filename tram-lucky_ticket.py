3bb0327805a7146982d99b235954f3f0e33fce85
Roman Khadraliev <Romch33tos@yandex.ru>
2025-07-01 21:45:23 +0700

    Создано окно приложения

---
+++ tram-lucky_ticket.py
@@ -0,0 +1,10 @@
+from tkinter import Tk
+
+class LuckyTramTicket:
+  def __init__(self):
+    self.window = Tk()
+    self.window.title("Счастливый билет")
+    self.window.mainloop()
+
+if __name__ == "__main__":
+  app = LuckyTramTicket()
