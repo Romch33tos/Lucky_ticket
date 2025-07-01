435e78c3fd5d3f6363719c1d0f888ccf6fbc0cab
Roman Khadraliev <Romch33tos@yandex.ru>
2025-07-01 21:49:48 +0700

    Добавлены надписи

--- tram-lucky_ticket.py
+++ tram-lucky_ticket.py
@@ -1,4 +1,5 @@
-from tkinter import Tk
+from tkinter import Tk, Label
+import random
 
 class LuckyTramTicket:
   def __init__(self):
@@ -10,7 +11,45 @@ class LuckyTramTicket:
 
     self.transport_numbers = ['AА', 'АБ', 'АВ', 'АГ', 'АЖ', 'АЗ', 'АК', 'АЛ', 'АМ', 'АН', 'АП', 'АР', 'АС', 'АТ']
 
+    self.create_widgets()
     self.window.mainloop()
+
+  def create_widgets(self):
+    Label(
+      self.window,
+      text="Минтранс РФ",
+      font=("Calibri", 12),
+      bg="snow"
+    ).grid(row=0, column=0, sticky="nw", padx=130, pady=5)
+
+    Label(
+      self.window,
+      text="АО «КЭТК», г. Кемерово",
+      font=("Calibri", 14),
+      bg="snow"
+    ).grid(row=1, column=0, sticky="nw", padx=80, pady=5)
+
+    Label(
+      self.window,
+      text="ТРАМВАЙ, ТРОЛЛЕЙБУС",
+      font=("Calibri", 16, 'bold'),
+      bg="snow"
+    ).grid(row=2, column=0, sticky="nw", padx=60, pady=5)
+
+    Label(
+      self.window,
+      text="ЛЬГОТНЫЙ БИЛЕТ",
+      font=("Calibri", 14),
+      bg="snow"
+    ).grid(row=3, column=0, sticky="nw", padx=100, pady=5)
+
+    Label(
+      self.window,
+      text=random.choice(self.transport_numbers),
+      bg="snow",
+      fg="maroon1",
+      font=("Calibri", 16)
+    ).grid(row=4, column=0, sticky="nw", padx=40, pady=10)
 
 if __name__ == "__main__":
   app = LuckyTramTicket()
